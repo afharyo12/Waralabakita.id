6cb66c5a8d7403d3d89f89a4489d133d63982b55
afharyo <afharyodiwseno@gmail.com>
2025-09-27 20:14:35 +0700

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 h-full">
           <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
             <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo" />
-            <span className="font-merriweather self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            <span className="font-nunito-sans self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
           </a>
           <div className="flex gap-x-2 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             {/* Toggle diubah untuk menggunakan div anak, bukan pseudo-element, 
